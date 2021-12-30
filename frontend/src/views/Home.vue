36b9834e625664969bb17a96cc64a7ab71d6f076
sebastian orellana <sebastian.orellana.v@usach.cl>
2021-12-30 13:39:32 -0300

    Se crea la app vue

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/terremoto.png" style="height:400px">
+    <HelloWorld msg="Bienvenido a la App de Terremotos"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
