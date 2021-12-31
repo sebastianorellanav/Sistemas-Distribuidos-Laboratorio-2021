1cd69fd55c888779e3e9147e2fec61cb6ac16742
Gary <gary.simken@usach.cl>
2021-12-31 12:16:19 -0300

    feat: se crea ruta para estadisticas
    Se mejora la query para obhtener terremotos, se trabjaa la data y se obtienen estadisticas

---
+++ backend/app.py
@@ -0,0 +1,65 @@
+from operator import mod
+import re
+from typing import Text
+import requests
+from flask import Flask, jsonify, request,make_response
+import json
+
+
+app= Flask(__name__)
+
+"""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/cai"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+db.init_app(app)
+
+ma = Marshmallow(app)
+migrate= Migrate(app,db)
+"""
+
+
+def get_info(param):
+	
+	param["format"]="geojson"
+	request_data= requests.get("https://earthquake.usgs.gov/fdsnws/event/1/query",params=param)
+	request_data=request_data.json()
+	request_data=request_data["features"]
+	respuesta=[]
+	for x in request_data:
+		respuesta.append(x["properties"])
+	
+	return respuesta
+@app.route("/terremotos/estadisticas",methods=["GET"])
+def stats():
+	parametros={}
+	datos=get_info(parametros)
+	
+	totales=0
+	tsunamis=0
+	grado=0
+	alertas=[0,0,0,0,0]
+	for x in datos:
+		if x["mag"] != None:
+			grado+=x["mag"]
+			if x["tsunami"]==1:
+				tsunamis+=1
+			totales+=1
+	
+	tsunamis=(tsunamis*100)/totales
+	grado=(grado)/totales
+	
+	datos={
+		"totales":totales,
+		"terremoto_con_prob_tsunami":tsunamis,
+		"grado_promedio":grado,
+		"alertas_verdes":alertas[0],
+
+	}
+	
+	return jsonify(datos)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
