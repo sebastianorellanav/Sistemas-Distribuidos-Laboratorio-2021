728c46546ced4fe184f555505b987c0b21f894a4
Gary <gary.simken@usach.cl>
2022-01-16 16:41:18 -0300

    nuevos cambios

--- backend/consumer.py
+++ backend/consumer.py
@@ -1,14 +1,25 @@
 from kafka import KafkaConsumer
-
-TOPIC_NAME = 'test'
+from json import loads
+from datetime import datetime
+import psycopg2
+TOPIC_NAME = 'terremoto'
 
 #Crear el consumidor
-consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer(TOPIC_NAME,bootstrap_servers=['localhost:9092'],auto_offset_reset='earliest',enable_auto_commit=True,group_id='my-group',value_deserializer=lambda x: loads(x.decode('utf-8')))
+conn = psycopg2.connect( database="terremotos", user='postgres', password='postgres', host='127.0.0.1', port= '5432')
+cursor = conn.cursor()
 
 #Leer los mensajes
 for message in consumer:
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+	message = message.value
+ 
+	for terremoto in message:
+		try:
+			cursor.execute("INSERT INTO terremoto (id,mag,place,time,updated,tz,url,detail,felt,cdi,mmi,alert,status,tsunami,sig,net,code,ids,sources,types,nst,dmin,rms,gap,magType,tipe) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(terremoto["id"],terremoto["mag"],terremoto["place"],terremoto["time"],terremoto["updated"],terremoto["tz"],terremoto["url"],terremoto["detail"],terremoto["felt"],terremoto["cdi"],terremoto["mmi"],terremoto["alert"],terremoto["status"],terremoto["tsunami"],terremoto["sig"],terremoto["net"],terremoto["code"],terremoto["ids"],terremoto["sources"],terremoto["types"],terremoto["nst"],terremoto["dmin"],terremoto["rms"],terremoto["gap"],terremoto["magType"],terremoto["type"]))
+			conn.commit()
+			print("terremoto agregado: ",terremoto["id"])
+		except Exception as e:
+			print("no fue agregado con error",e,'\n',terremoto)
+
+
+conn.close()
