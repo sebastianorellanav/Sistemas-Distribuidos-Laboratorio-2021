c5f85c4172c07aff46040998daed3e2f616c9c95
sebastian orellana <sebastian.orellana.v@usach.cl>
2022-01-20 21:20:54 -0300

    Fixes

--- frontend/src/views/LatestEarthquakes.vue
+++ frontend/src/views/LatestEarthquakes.vue
@@ -160,7 +160,7 @@ export default {
 
     created() {
         this.isLoading = true;
-        let url_backend = "http://40.78.155.134:5000";
+        let url_backend = "http://localhost:5000";
         let endpoint = "/terremotos";
         console.log("searching...");
         console.log(url_backend + endpoint);
